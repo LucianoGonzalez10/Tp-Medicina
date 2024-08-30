b5ab9a6f24c85b2d32f7cbece23544992a0ae5ec
Luciano <luchogoma342@gmail.com>
2024-08-30 16:57:15 -0300

    Act

--- app.js
+++ app.js
@@ -65,9 +65,13 @@ class Inventario{
 
 let inv = new Inventario();
 let med1 = new Medicamento("2", "Ibuprofeno", 299, 3);
+let med2 = new Medicamento("1", "Paracetamol", 299, 2);
+let med3 = new Medicamento("5", "Viagra", 299, 1);
+let med4 = new Medicamento("7", "Pastilla para dormir", 299, 0);
 inv.agregarMedicamento(med1);
-inv.mostrarInventario();
-inv.actualizarStock(1, 7);
+inv.agregarMedicamento(med2);
+inv.agregarMedicamento(med3);
+inv.agregarMedicamento(med4);
 inv.mostrarInventario();
 
 class Venta{
@@ -84,8 +88,15 @@ class Venta{
     }
 }
 
-let venta = new Venta(med1, 2);
-venta.registrarVenta();
-inv.mostrarInventario();
+generarReporte = (inventario) => {
+    if(inventario instanceof Inventario){
+      inventario.a.forEach(element => {
+        if(element.stock < 3){
+            console.log(`El medicamento con el ID: ${element.id} y nombre: ${element.nombre} tiene bajo stock.`);
+        }
+      });
+    }
+}
 
+generarReporte(inv);
 
